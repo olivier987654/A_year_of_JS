30c09c38c9a1292e07eb24b03136c9e75df797ef
Olivier Gendron <112414219+olivier987654@users.noreply.github.com>
2022-10-15 08:35:19 -0700

    Initial Commit
    Initial Commit

---
+++ 2022-10-15/2022_10_15.js
@@ -0,0 +1,9 @@
+// Reversing an array in JavaScript
+
+var arr = [1, 2`3, 4, 5, 6, 7, 8, 9, 10];
+var reversed = arr.reverse();
+
+console.log(reversed);
+
+// Output: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
---
+++ 2022-10-15/2022_10_15_2.js
@@ -0,0 +1,12 @@
+// JavaScript Object Testing 
+
+var identity = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+if (identity.name == "John") {
+    console.log("Hello John");
+}
+
+
